522e7acdd7b0dc2999650df407609342e1298364
khayalfarzi <xeyalferziyev9@gmail.com>
2020-05-28 14:50:15 +0300

    if else

---
+++ src/main/java/az/company/firstjava/lesson5/FlowControl.java
@@ -0,0 +1,60 @@
+package az.company.firstjava.lesson5;
+
+public class FlowControl {
+
+    public static void main(String[] args) {
+/** task 1*/
+//        boolean isFaster;
+//        boolean isSlower = false;
+//        int result = 0;
+//
+//        if (isFaster = true || (isSlower = true)) {
+//            result++;
+//            isFaster = false; //false
+//        }
+//        if (isFaster || isSlower && (isSlower = false || isFaster)) {
+//            result++;
+//        }
+//        if (!isFaster && !isSlower) {
+//            result++;
+//        }
+//        System.out.println(String.valueOf(result) + isFaster + isSlower);
+//
+
+        /** task 2*/
+//        String str = "GeeksforGeeks";
+//        int i = 4;
+//
+//        // if block
+//        if (i == 4) {
+//            i++;
+//            System.out.println(str);
+//        }
+//
+//        // Executed by default
+//        System.out.println("i = " + i);
+//
+//        /** task 3 */
+//        int number = 10;
+//
+//        // checks if number is greater than 0
+//        if (number > 0) {
+//            System.out.println("The number is positive.");
+//        }
+//        System.out.println("This statement is always executed.");
+
+        /** task 4 */
+//        int number = 10;
+//
+//        // checks if number is greater than 0
+//        if (number > 0) {
+//            System.out.println("The number is positive.");
+//        }
+//        else {
+//            System.out.println("The number is not positive.");
+//        }
+//
+//        System.out.println("This statement is always executed.");
+
+    }
+}
