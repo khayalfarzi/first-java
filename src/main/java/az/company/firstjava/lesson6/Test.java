6991cc12142ca45c9742916e99d537aa1ce271a5
khayalfarzi <xeyalferziyev9@gmail.com>
2020-07-12 09:59:03 +0300

    switch,fori,while explanetion

---
+++ src/main/java/az/company/firstjava/lesson6/Test.java
@@ -0,0 +1,21 @@
+package az.company.firstjava.lesson6;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(" Pakize ");
+        int num = 8;
+
+        if (num>0){
+            System.out.println("bu eded musbetdir");
+        }
+        else if (num<0){
+            System.out.println("bu eded menfidir");
+        }
+        else {
+            System.out.println("bu eded sifira beraberdir");
+        }
+    }
+}
---
+++ src/main/java/az/company/firstjava/test/Main.java
@@ -0,0 +1,14 @@
+package az.company.firstjava.test;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int max = Math.max(5, 89);
+        int min = Math.min(5, 98);
+        double kokAlti = Math.sqrt(121);
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(kokAlti);
+    }
+}
